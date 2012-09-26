781e6cabdf09b1e573bb4fca5d8ba2672171e8c1
quocle <quocle@exampleemail.org>
2012-09-25 23:14:34 -0400

    Add initializer to Suite Result

--- app/models/suite_result.rb
+++ app/models/suite_result.rb
@@ -2,4 +2,9 @@ class SuiteResult < ActiveRecord::Base
   attr_accessible :run_id, :status, :test_suite_id
   belongs_to :run
   belongs_to :test_suite
+  after_initialize :init
+  
+  def init
+    self.status ||= 0
+  end
 end
--- spec/models/suite_result_spec.rb
+++ spec/models/suite_result_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe "Suite Result" do
+  describe "build" do 
+    let!(:suite_result){SuiteResult.new}
+    it "should initialize the value of status to 0" do
+      suite_result.status.should eq(0)
+    end
+  end
+  
   describe "create" do
     let!(:suite_result){FactoryGirl.create(:suite_result)}
     it "should belong to run" do
@@ -10,7 +17,6 @@ describe "Suite Result" do
     it "should belong to test suite" do
       suite_result.should belong_to(:test_suite)
     end
-    
   end
 
 end

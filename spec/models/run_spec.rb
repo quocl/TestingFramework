ddbcc88ac450849f4d200fd21c79affb7986dd59
quocle <quocle@exampleemail.org>
2012-09-25 23:09:04 -0400

    Add more tests

--- spec/models/run_spec.rb
+++ spec/models/run_spec.rb
@@ -7,4 +7,13 @@ describe "Run" do
       run.should have_many(:suite_results)
     end
   end
+  
+  describe "destroy" do
+    let!(:suite_result){FactoryGirl.create(:suite_result)}   
+    it "should destroy the suite result belongs to the test suite" do
+      SuiteResult.all.should include(suite_result)
+      suite_result.run.destroy
+      SuiteResult.all.should_not include(suite_result)
+    end 
+  end
 end
--- spec/models/test_suite_spec.rb
+++ spec/models/test_suite_spec.rb
@@ -16,4 +16,12 @@ describe "Test Suite" do
     end
   end
 
+  describe "destroy" do
+    let!(:suite_result){FactoryGirl.create(:suite_result)}   
+    it "should destroy the suite result belongs to the test suite" do
+      SuiteResult.all.should include(suite_result)
+      suite_result.test_suite.destroy
+      SuiteResult.all.should_not include(suite_result)
+    end 
+  end
 end
